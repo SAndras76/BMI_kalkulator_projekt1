7223922b99d854284fbdbdbabe992b8c217e6626
SAndras76 <andras.java@gmail.com>
2022-08-18 12:15:16 +0200

    Service

--- src/main/java/hu/progmatic/bmi_kalkulator_projekt/controllers/BmiController.java
+++ src/main/java/hu/progmatic/bmi_kalkulator_projekt/controllers/BmiController.java
@@ -1,8 +1,17 @@
 package hu.progmatic.bmi_kalkulator_projekt.controllers;
 
+import hu.progmatic.bmi_kalkulator_projekt.services.BmiService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class BmiController {
 
+    private BmiService bmiService;
+
+
+    @Autowired
+    public BmiController(BmiService bmiService){
+        this.bmiService = bmiService;
+    }
 }
--- src/main/java/hu/progmatic/bmi_kalkulator_projekt/services/BmiService.java
+++ src/main/java/hu/progmatic/bmi_kalkulator_projekt/services/BmiService.java
@@ -1,4 +1,14 @@
 package hu.progmatic.bmi_kalkulator_projekt.services;
 
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Service
 public class BmiService {
+    @PersistenceContext
+    EntityManager entityManager;
+
+
 }
